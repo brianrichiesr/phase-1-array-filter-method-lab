2f31f52f8e2a76c98ed3605a73c7e9a4bf175de9
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-15 10:33:57 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const findMatching = (arr, str) => {
+    return arr.filter((each) => {
+        return each.toLowerCase() === str.toLowerCase();
+    })
+}
+
+const fuzzyMatch = (arr, str) => {
+    return arr.filter((each) => {
+        return each[0].toLowerCase() === str[0].toLowerCase();
+    })
+}
+
+const matchName = (arr, str) => {
+    return arr.filter ((each) => {
+        return each.name === str;
+    })
+}
